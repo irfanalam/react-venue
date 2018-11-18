4e1dbdd8550c0bf29072cdb1ccaef9b05641af26
irfanalam <irfanalam49@gmail.com>
2018-11-18 20:53:43 +0500

    venue info component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './resources/styles.css';
 
 //components
 import Header from './components/header_footer/header';
-import Featured from './components/featured'
+import Featured from './components/featured';
+import VenueInfo from './components/VenueInfo';
+
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height: '1500px', backgroundColor: 'cornflowerBlue'}}>
         <Header />
-        <Featured /> 
+        <Featured />
+        <VenueInfo />
       </div>
     );
   }
